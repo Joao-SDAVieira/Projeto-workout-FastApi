dd373bffbf2a1b9239727f1ef23c85bfcdd300f7
João <eujoaovictor0707@gmail.com>
2025-01-22 18:42:18 -0300

    Criado rotas Get e Post para Centro de Treinamento

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,8 +1,14 @@
 from workout_api.contrib.schemas import BaseSchema
-from pydantic import Field
+from pydantic import Field, UUID4
 from typing import Annotated
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', examples=['CT King'], max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do centro de treinamento', examples=['Rua X, Q02'], max_length=60)] #erro de tipo no examples
-    poprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', examples=['Marcos'], max_length=30)]
+    proprietario: Annotated[str, Field(description='Proprietario do centro de treinamento', examples=['Marcos'], max_length=30)]
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', examples=['CT King'], max_length=20)]
+    
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador do centro de treinamento')]
